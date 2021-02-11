8a9de514711728dee49da5a22d87f33a831e8815
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-11 18:48:34 +0530

    [vijay]BoardCreation

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    public class TicTactoe
+    {
+        /// <summary>
+        /// Created a Board with size 10 char array
+        /// </summary>
+        public static char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        public static void createBoard()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(" Empty board:" + i + ": " + board[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game !");
+            TicTactoe.createBoard();
+        }
+    }
+}
