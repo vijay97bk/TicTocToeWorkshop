b581844ae43fa3ca1b80b22ee638be61792fe8d9
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-11 18:52:49 +0530

    [vijay]LetterOptionXO

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,6 +4,10 @@ namespace TicTacToeGame
 {
     public class TicTactoe
     {
+        public static string PLAYER_1;
+        public const int LETTERX = 0;
+        public const int LETTERO = 1;
+
         /// <summary>
         /// Created a Board with size 10 char array
         /// </summary>
@@ -15,6 +19,37 @@ namespace TicTacToeGame
                 Console.WriteLine(" Empty board:" + i + ": " + board[i]);
             }
         }
+        /// <summary>
+        /// Choose a Letter fro player and computer
+        /// </summary>
+        public static void chooseLetter()
+        {
+
+            Random rand = new Random();
+            int letterComputer = rand.Next(0, 2);
+            Console.WriteLine(" Choose a Letter");
+            int letterPlayer = Convert.ToInt32(Console.ReadLine());
+
+            switch (letterPlayer)
+            {
+                case LETTERX:
+                    Console.WriteLine(" Player choose X Option ");
+                    break;
+                case LETTERO:
+                    Console.WriteLine("  Player choose O Option  ");
+                    break;
+            }
+
+            switch (letterComputer)
+            {
+                case LETTERX:
+                    Console.WriteLine(" Computer choose X Option ");
+                    break;
+                case LETTERO:
+                    Console.WriteLine(" Computer choose O Option  ");
+                    break;
+            }
+        }
     }
     class Program
     {
@@ -22,6 +57,7 @@ namespace TicTacToeGame
         {
             Console.WriteLine("Welcome to TicTacToe Game !");
             TicTactoe.createBoard();
+            TicTactoe.chooseLetter();
         }
     }
 }
