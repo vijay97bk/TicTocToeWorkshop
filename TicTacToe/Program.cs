49f7522d2a3706eccf8a95120ea6238168ce11cc
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-11 19:30:23 +0530

    [vijay]AbilityToPlayAnotherGame

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,6 +4,7 @@ namespace TicTacToeGame
 {
     class TicTacToe
     {
+
         public static string PLAYER_1;
         public const char LETTERX = 'X';
         public const char LETTERO = 'O';
@@ -15,6 +16,17 @@ namespace TicTacToeGame
                 Console.WriteLine(" Empty board:" + i + ": " + board[i]);
             }
         }
+
+        public static void emptyTheBoard()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+                Console.WriteLine(" Empty board:" + i + ": " + board[i]);
+            }
+        }
+
+
 
         //public static char chooseLetter(char letter)
         //{
@@ -48,105 +60,94 @@ namespace TicTacToeGame
 
         }
 
-        public static void makeAMove(char[] letter)
+        public static void play(char[] letter)
+        {
+            const int HEAD = 0;
+            const int TAIL = 1;
+            char freeSpace = ' ';
+            Random rand = new Random();
+            switch (makeAToss())
+            {
+                case HEAD:
+                    Console.WriteLine(" Player wins a toss ");
+                    while (board[1] == ' ' || board[2] == ' ' || board[3] == ' ' || board[4] == ' ' || board[5] == ' ' || board[6] == ' ' || board[7] == ' ' || board[8] == ' ' || board[9] == ' ')
+                    {
+                        player(letter);
+                        if (isWin(letter) == true)
+                        {
+                            Console.WriteLine(" Player Wins ");
+                            break;
+                        }
+                        computer(letter);
+                        if (isWin(letter) == true)
+                        {
+                            Console.WriteLine(" Computer Wins ");
+                            break;
+                        }
+                    }
+                    break;
+                case TAIL:
+                    Console.WriteLine(" Player wins a toss ");
+                    while (board[1] == ' ' || board[2] == ' ' || board[3] == ' ' || board[4] == ' ' || board[5] == ' ' || board[6] == ' ' || board[7] == ' ' || board[8] == ' ' || board[9] == ' ')
+                    {
+                        computer(letter);
+                        if (isWin(letter) == true)
+                        {
+                            Console.WriteLine(" Computer Wins ");
+                            break;
+                        }
+                        player(letter);
+                        if (isWin(letter) == true)
+                        {
+                            Console.WriteLine(" Player Wins ");
+                            break;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public static void player(char[] letter)
         {
             const int CORNER = 1;
             const int CENTRE = 2;
             const int SIDES = 3;
-            char freeSpace = ' ';
-            Random rand = new Random();
-            if (makeAToss() == 1)
+            Console.WriteLine(" Players Move ");
+            Console.WriteLine(" Choose option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Make a move ");
+            int indexPlayer = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-                Console.WriteLine(" Player wins a toss ");
-                while (board[1] == ' ' || board[2] == ' ' || board[3] == ' ' || board[4] == ' ' || board[5] == ' ' || board[6] == ' ' || board[7] == ' ' || board[8] == ' ' || board[9] == ' ')
-                {
-                    Console.WriteLine(" Players Move ");
-                    Console.WriteLine(" Choose option: ");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(" Make a move ");
-                    int indexPlayer = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
-                    {
-                        case SIDES:
-                            Console.WriteLine(" Move to availabel sides ");
-                            availabelSides(letter, indexPlayer);
-                            break;
-                        case CORNER:
-                            Console.WriteLine(" Move to availabel corner ");
-                            availabelCorners(letter, indexPlayer);
-                            break;
-                        case CENTRE:
-                            Console.WriteLine(" Move to availabel centre ");
-                            availabelCentre(letter);
-                            break;
-                    }
-                    showBoard();
-                    if (isWin(letter) == true)
-                    {
-                        Console.WriteLine(" Player Wins ");
-                        Environment.Exit(0);
-                    }
-                    Console.WriteLine(" Computers Move ");
-                    int indexComputer = rand.Next(1, 10);
-                    if (board[indexComputer] == ' ')
-                    {
-                        board[indexComputer] = letter[1];
-                        if (isWin(letter) == true)
-                        {
-                            Console.WriteLine(" Computer Wins ");
-                            Environment.Exit(0);
-                        }
-                    }
-                    showBoard();
-                }
+                case SIDES:
+                    Console.WriteLine(" Move to availabel sides ");
+                    availabelSides(letter, indexPlayer);
+                    break;
+                case CORNER:
+                    Console.WriteLine(" Move to availabel corner ");
+                    availabelCorners(letter, indexPlayer);
+                    break;
+                case CENTRE:
+                    Console.WriteLine(" Move to availabel centre ");
+                    availabelCentre(letter);
+                    break;
+            }
+            showBoard();
 
-            }
-            else
-            {
-                Console.WriteLine(" Computer wins a toss ");
-                while (board[1] == ' ' || board[2] == ' ' || board[3] == ' ' || board[4] == ' ' || board[5] == ' ' || board[6] == ' ' || board[7] == ' ' || board[8] == ' ' || board[9] == ' ')
-                {
-                    Console.WriteLine(" Computers Move ");
-                    int indexComputer = rand.Next(1, 10);
-                    if (board[indexComputer] == ' ')
-                    {
-                        board[indexComputer] = letter[1];
-                        if (isWin(letter) == true)
-                        {
-                            Console.WriteLine(" Computer Wins ");
-                            Environment.Exit(0);
-                        }
-                    }
-                    showBoard();
-                    Console.WriteLine(" Players Move ");
-                    Console.WriteLine(" Choose option: ");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(" Make a move ");
-                    int indexPlayer = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
-                    {
-                        case SIDES:
-                            Console.WriteLine(" Move to availabel sides ");
-                            availabelSides(letter, indexPlayer);
-                            break;
-                        case CORNER:
-                            Console.WriteLine(" Move to availabel corner ");
-                            availabelCorners(letter, indexPlayer);
-                            break;
-                        case CENTRE:
-                            Console.WriteLine(" Move to availabel centre ");
-                            availabelCentre(letter);
-                            break;
-                    }
-                    showBoard();
-                    if (isWin(letter) == true)
-                    {
-                        Console.WriteLine(" Player Wins ");
-                        Environment.Exit(0);
-                    }
-                }
-            }
         }
+
+        public static void computer(char[] letter)
+        {
+            Random rand = new Random();
+            Console.WriteLine(" Computers Move ");
+            int indexComputer = rand.Next(1, 10);
+            if (board[indexComputer] == ' ')
+            {
+                board[indexComputer] = letter[1];
+            }
+            showBoard();
+        }
+
         public static bool isWin(char[] letter)
         {
             return (board[1] == letter[0] && board[2] == letter[0] && board[3] == letter[0]) ||
@@ -177,7 +178,7 @@ namespace TicTacToeGame
 
         public static void availabelSides(char[] letter, int indexPlayer)
         {
-            if ((board[2] == ' ' || board[4] == ' ' || board[6] == ' ' || board[8] == ' ') && (board[1] != ' ' && board[3] != ' ' && board[7] != ' ' && board[9] != ' ' && board[5] != ' '))
+            if ((board[2] == ' ' || board[4] == ' ' || board[6] == ' ' || board[8] == ' ') || (board[1] != ' ' && board[3] != ' ' && board[7] != ' ' && board[9] != ' ' && board[5] != ' '))
             {
                 board[indexPlayer] = letter[0];
             }
@@ -185,7 +186,7 @@ namespace TicTacToeGame
 
         public static void availabelCentre(char[] letter)
         {
-            if (board[5] == ' ' && (board[1] != ' ' && board[3] != ' ' && board[7] != ' ' && board[9] != ' '))
+            if (board[5] == ' ' || (board[1] != ' ' && board[3] != ' ' && board[7] != ' ' && board[9] != ' '))
             {
                 int indexPlayer = 5;
                 board[indexPlayer] = letter[0];
@@ -210,9 +211,14 @@ namespace TicTacToeGame
             Console.WriteLine(" Welcome to Tic Tac Toe Game");
 
             // TicTacToe.chooseLetter(letter);   
-            TicTacToe.makeAMove(letter);
-
-
+            TicTacToe.play(letter);
+            string anotherGame = Console.ReadLine();
+            if (anotherGame == "YES")
+            {
+                Console.WriteLine(" Play Another Game ");
+                TicTacToe.emptyTheBoard();
+                TicTacToe.play(letter);
+            }
         }
     }
 }
